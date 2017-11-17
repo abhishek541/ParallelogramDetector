63d6cbff8786a247c01b6dfbfb2512e1d0497628
abhishek541 <ak6210@nyu.edu>
2017-11-16 21:43:48 -0500

    Added non maxima suppression for edge detection

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PIL import Image
-import math
+from math import sqrt, hypot, pi, cos, sin, atan2
+from numpy import array, zeros
 
-im = Image.open('test-images\TestImage1c.jpg')
+im = Image.open('test-images\TestImage.jpg')
 pix = im.load()
 print(im.format, im.size, im.mode)
 
@@ -49,10 +50,6 @@ for indexi, i in enumerate(img_arr):
             if indexi < width - 1:
                 lowerright = img_arr[indexi + 1][indexj + 1] * -1
         val = upperleft + upperright + left + right + lowerleft + lowerright
-        if val > 255:
-            val = 255
-        if val < 0:
-            val = 0
         sobelrowx.append(val)
     sobelimagex.append(sobelrowx)
 
@@ -86,10 +83,6 @@ for indexi, i in enumerate(img_arr):
             if indexj < height - 1:
                 lowerright = img_arr[indexi + 1][indexj + 1] * -1
         val = upperleft + upperright + up + low + lowerleft + lowerright
-        if val > 255:
-            val = 255
-        if val < 0:
-            val = 0
         sobelrowy.append(val)
     sobelimagey.append(sobelrowy)
 
@@ -104,11 +97,95 @@ im_y.putdata(sobelarry)
 #im_y.show()
 
 im_gradarr = []
+im_gradang = zeros(im.size)
+im_gradmag = zeros(im.size)
 for m in range(0, height):
     for l in range(0, width):
-        grad_val = math.sqrt(sobelimagex[l][m]**2 + sobelimagey[l][m]**2)
+        grad_val = int(sqrt(sobelimagex[l][m]**2 + sobelimagey[l][m]**2))
+        ang_val = atan2(sobelimagey[l][m], sobelimagex[l][m])
+        if ang_val < 0:
+            ang_val = ang_val + 180
         im_gradarr.append(grad_val)
+        im_gradmag[l][m] = grad_val
+        im_gradang[l][m] = ang_val
+
+im_normarr = []
+for n in range(0, len(im_gradarr)):
+    newVal = (im_gradarr[n] - min(im_gradarr))/(max(im_gradarr) - min(im_gradarr)) * 255
+    print(str(n))
+    im_normarr.append(newVal)
+
+im_normmag = zeros(im.size)
+for m in range(0, height):
+    for l in range(0, width):
+        normVal = (im_gradmag[l][m] - min(im_gradarr)) / (max(im_gradarr) - min(im_gradarr)) * 255
+        im_normmag[l][m] = normVal
 
 im_grad = Image.new('L', im.size)
-im_grad.putdata(im_gradarr)
+im_grad.putdata(im_normarr)
+print(str(max(im_normarr)))
 im_grad.show()
+
+im_nms = zeros(im.size)
+for j in range(0, height):
+    for i in range(0, width):
+        if ((j+1) < height) and ((j-1) >= 0) and ((i+1) < width) and ((i-1) >= 0):
+            # 0 degrees
+            if (im_gradang[i][j] >= 337.5 or im_gradang[i][j] < 22.5) or (im_gradang[i][j] >= 157.5 and im_gradang[i][j] < 202.5):
+                if im_normmag[i][j] >= im_normmag[i][j + 1] and im_normmag[i][j] >= im_normmag[i][j - 1]:
+                    im_nms[i][j] = im_normmag[i][j]
+            # 45 degrees
+            if (im_gradang[i][j] >= 22.5 and im_gradang[i][j] < 67.5) or (im_gradang[i][j] >= 202.5 and im_gradang[i][j] < 247.5):
+                if im_normmag[i][j] >= im_normmag[i - 1][j + 1] and im_normmag[i][j] >= im_normmag[i + 1][j - 1]:
+                    im_nms[i][j] = im_normmag[i][j]
+            # 90 degrees
+            if (im_gradang[i][j] >= 67.5 and im_gradang[i][j] < 112.5) or (im_gradang[i][j] >= 247.5 and im_gradang[i][j] < 292.5):
+                if im_normmag[i][j] >= im_normmag[i - 1][j] and im_normmag[i][j] >= im_normmag[i + 1][j]:
+                    im_nms[i][j] = im_normmag[i][j]
+            # 135 degrees
+            if (im_gradang[i][j] >= 112.5 and im_gradang[i][j] < 157.5) or (im_gradang[i][j] >= 292.5 and im_gradang[i][j] < 337.5):
+                if im_normmag[i][j] >= im_normmag[i - 1][j - 1] and im_normmag[i][j] >= im_normmag[i + 1][j + 1]:
+                    im_nms[i][j] = im_normmag[i][j]
+
+im_nmsarr = []
+for m in range(0, height):
+    for l in range(0, width):
+        im_nmsarr.append(im_nms[l][m])
+
+im_trarr = []
+for y in range(0, height):
+    for x in range(0, width):
+        v = im_grad.getpixel((x,y))
+        if v <= 40:
+            v = 255
+        else:
+            v = 0
+        im_trarr.append(v)
+
+im_tr = Image.new('L', im.size)
+im_tr.putdata(im_nmsarr)
+im_tr.show()
+
+# pim = im_tr.load()
+# ntx, mry = 460, 360
+# nimx, mimy = im_tr.size
+# mry = int(mry / 2) * 2  # Make sure that this is even
+# him = Image.new("L", (ntx, mry), 255)
+# phim = him.load()
+#
+# rmax = hypot(nimx, mimy)
+# dr = rmax / (mry / 2)
+# dth = pi / ntx
+#
+# for jx in range(nimx):
+#     print("in for: "+str(jx))
+#     for iy in range(mimy):
+#         col = pim[jx, iy]
+#         if col == 255: continue
+#         for jtx in range(ntx):
+#             th = dth * jtx
+#             r = jx * cos(th) + iy * sin(th)
+#             iry = mry / 2 + int(r / dr + 0.5)
+#             phim[jtx, iry] -= 1
+#
+# him.show()
